7225b35298becc892714da1433d01c54b61eb6f4
stefano soro <stefano_soro@beirario.com.br>
2021-03-07 15:26:57 -0300

    :sparkles: Add collor pallete

---
+++ src/styles/colors.ts
@@ -0,0 +1,11 @@
+export default {
+  white: '#FFFFFF',
+  gelo: '#F9F9F9',
+  gray: '#565657',
+  black: '#000000',
+  green1: '#F1FFE7',
+  green2: '#A9FDAC',
+  green3: '#44CF6C',
+  green4: '#32A287',
+  brown: '#6C464E',
+};
