193e5a60fb3b7d2a17df85ba1efa00ca96d8abd2
stefano soro <stefano_soro@beirario.com.br>
2021-06-11 16:50:11 -0300

    :recycle: Code refactoring

--- src/services/sqlite/config/index.ts
+++ src/services/sqlite/config/index.ts
@@ -5,7 +5,7 @@ import { UserEntity } from '../entity/user.entity';
 export const mountConnection = async (): Promise<Connection> => {
   return createConnection({
     type: 'react-native',
-    database: 'test',
+    database: 'pokedex-db',
     location: 'default',
     logging: ['error', 'query', 'schema'],
     synchronize: true,
